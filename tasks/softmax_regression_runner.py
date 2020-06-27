3b4da047a3af35f5e9ccc5a45dc95a92b1d3c548
Alexander Malyscheff <malyscheff@gmail.com>
2020-06-27 20:51:07 +0200

    Rename run_handler_softmax_vec

--- tasks/softmax_regression_runner.py
+++ tasks/softmax_regression_runner.py
@@ -1,15 +1,16 @@
 """
 runfile('softmax_regression_runner.py', wdir='/home/admin_thinkpad/Git/dl_repository/tasks')
 """
-from modules import run_handler_softmax
+from modules import run_handler_softmax_vec
 import time as mytime
 
 start_time = mytime.time()
 input_file_path = r'/home/admin_thinkpad/Git/dl_repository/tests/Case01/Inputs//'
-
-result = run_handler_softmax.main(train_images_fname=input_file_path + 'train-images-idx3-ubyte',
-                                  train_labels_fname=input_file_path + 'train-labels-idx1-ubyte',
-                                  test_images_fname=input_file_path + 't10k-images-idx3-ubyte',
-                                  test_labels_fname=input_file_path + 't10k-labels-idx1-ubyte')
+vec_flag = True
+if vec_flag:
+    result = run_handler_softmax_vec.main(train_images_fname=input_file_path + 'train-images-idx3-ubyte',
+                                          train_labels_fname=input_file_path + 'train-labels-idx1-ubyte',
+                                          test_images_fname=input_file_path + 't10k-images-idx3-ubyte',
+                                          test_labels_fname=input_file_path + 't10k-labels-idx1-ubyte')
 
 print("Overall time: {}".format(round(mytime.time() - start_time, 2)))
